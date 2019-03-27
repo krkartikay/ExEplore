fb604e9b17ae566bc948045bb14c3447d24ee092
Lakshay Gupta <lakshaygpt28@gmail.com>
2019-03-27 19:42:51 +0530

    breakout added

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from flask import *
 from flask_sqlalchemy import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:shubh@123@localhost/exeplore"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:123@localhost/exeplore"
 db = SQLAlchemy(app)
 app.secret_key = "random"
