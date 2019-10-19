758ccc150ebbdc49442ea74c1de39f7e55f481e3
Chirag Thakur <chirag11032000@gmail.com>
2019-10-19 09:48:20 +0530

    changed config

--- exeplore/config.py
+++ exeplore/config.py
@@ -2,7 +2,7 @@ from flask import *
 from flask_sqlalchemy import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Samakaria123#@localhost/exeplore"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:password@localhost/exeplore"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 app.secret_key = "DontRemember!"
